1015f4fb07c47bc076eef926f616c93e4ec3cbea
Devesh Kumar <dkshandilya12345@gmail.com>
2024-02-03 02:02:26 +0530

    final

--- Blogram/settings.py
+++ Blogram/settings.py
@@ -114,6 +114,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -133,3 +135,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dkshandilya12345@gmail.com'
+EMAIL_HOST_PASSWORD = 'qozq gpvz twcz kwzm'
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
